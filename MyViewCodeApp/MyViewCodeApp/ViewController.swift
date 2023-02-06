7e8474e7c389d108e6cdf10cd01af79e46e225d1
lclaudiomattoss <lclaudiomattoss@gmail.com>
2023-02-06 10:11:56 -0300

    Criação do projeto MyViewCodeApp
    Criado o projeto MyViewCodeApp no XCode.

---
+++ MyViewCodeApp/MyViewCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyViewCodeApp
+//
+//  Created by Luiz Claudio Mattos da Silva on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
